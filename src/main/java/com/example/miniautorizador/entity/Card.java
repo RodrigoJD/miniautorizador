699bb0e4c07e0b3c06c50b66370bc2a61ab86380
Rodrigo <rodjefdam@gmail.com>
2024-09-25 10:54:41 -0300

    romovida propriedade para trabalhar com imagem do container do mysql, ajustado tipos dos atributos da entidade card

--- src/main/java/com/example/miniautorizador/entity/Card.java
+++ src/main/java/com/example/miniautorizador/entity/Card.java
@@ -3,17 +3,15 @@ package com.example.miniautorizador.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Entity
 @Data
 public class Card {
 
     @Id
-    private Integer cardNumber;
+    private Long cardNumber;
 
-    private Integer password;
+    private String password;
 
     private Double balance;
 }
